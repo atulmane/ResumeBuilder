07c20df5341dce40aac6889bd97fa5b9d90bab25
atul_mane01 <maneatul2116@gmail.com>
2021-08-08 14:51:32 +0530

    changes

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from app import views
 urlpatterns = [
-   path("", views.resume, name='resume'),
+   path("", views.index, name='index'),
 
 ]
